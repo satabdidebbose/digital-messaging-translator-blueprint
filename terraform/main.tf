8d51f2687b1d03e16a4ec7261eafb148bf984e2b
satabdidebbose <163126223+satabdidebbose@users.noreply.github.com>
2025-05-13 19:38:21 +0530

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    genesyscloud = {
+      source  = "mypurecloud/genesyscloud",
+      version = ">= 1.6.0"
+    }
+  }
+}
+
+provider "genesyscloud" {
+  oauthclient_id = "<client-id>"
+  oauthclient_secret = "<client-secret>"
+  aws_region = "<aws-region>"
+}
